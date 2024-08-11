7c11db72d8a87632c993fc1f54e083410a67670a
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2024-08-10 23:45:09 -0300

    terminado House of the dragon - Semana do zero ao programador contratado

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const button = document.querySelectorAll('.button');
+const img = document.querySelectorAll('.img');
+const informacoes = document.querySelectorAll('.informacoes');
+
+button.forEach((button, indice) => {//ForEach => para cada button acontce algo 
+    button.addEventListener('click', () => {
+        removeButton();
+        button.classList.add("selecionado")
+
+        removeImg();   
+        img[indice].classList.add("ativa")//indice consigo pegar a posição
+
+        removeInformacoes();   
+        informacoes[indice].classList.add("ativa")
+        
+    })
+});
+
+function removeInformacoes() {
+    const informaAtiva = document.querySelector(".informacoes.ativa");
+    informaAtiva.classList.remove("ativa");
+}
+
+function removeImg() {
+    const ImagemAtiva = document.querySelector(".ativa");
+    ImagemAtiva.classList.remove("ativa");
+}
+
+function removeButton() {
+    const buttonSelector = document.querySelector(".selecionado");
+    buttonSelector.classList.remove("selecionado");
+}
